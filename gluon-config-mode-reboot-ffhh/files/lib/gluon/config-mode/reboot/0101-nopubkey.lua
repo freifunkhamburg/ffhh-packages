818765060bc76a109dde1558513fd086e5973ef1
bjoern <bdobe@users.noreply.github.com>
2015-04-11 09:53:14 +0200

    gluon-config-mode-reboot-ffhh: add i18n support

--- gluon-config-mode-reboot-ffhh/files/lib/gluon/config-mode/reboot/0101-nopubkey.lua
+++ gluon-config-mode-reboot-ffhh/files/lib/gluon/config-mode/reboot/0101-nopubkey.lua
@@ -1,17 +1,16 @@
-local msg = [[<p>
-               <%= luci.template.render_string(site.config_mode.msg_nopubkey) %>
-             </p>]]
-
 local uci = luci.model.uci.cursor()
 local meshvpn_enabled = uci:get("fastd", "mesh_vpn", "enabled", "0")
 
 if meshvpn_enabled == "1" then
   return nil
 else
+  local i18n = require "luci.i18n"
   local site = require 'gluon.site_config'
   local sysconfig = require 'gluon.sysconfig'
 
   local hostname = uci:get_first("system", "system", "hostname")
+
+  local msg = [[<p>]] .. i18n.translate('gluon-config-mode:nopubkey') .. [[</p>]]
 
   return function ()
            luci.template.render_string(msg, { hostname=hostname
